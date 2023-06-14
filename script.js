9df9d7c86ac3ace49b0735e9a4185f17a6564631
Mohsin Zia <75737591+mohsinziaa@users.noreply.github.com>
2023-06-14 08:27:36 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,40 +3,40 @@
 // Data
 const account1 = {
   owner: "Mohsin Zia",
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
+  movements: [200.2, 450.6, -400.82, 3000, -650.5, -130, 70, 1300.1],
+  interestRate: 1.2,
   pin: 244,
   movementsDates: [
-    "2023-07-01T10:51:36.790Z",
-    "2023-07-03T23:36:17.929Z",
-    "2023-07-06T17:01:17.194Z",
-    "2023-07-08T14:11:59.604Z",
-    "2023-07-09T10:17:24.185Z",
-    "2023-07-10T09:15:04.904Z",
-    "2023-07-11T07:42:02.383Z",
-    "2023-07-12T21:31:17.178Z",
+    "2023-06-01T10:51:36.790Z",
+    "2023-06-03T23:36:17.929Z",
+    "2023-06-06T17:01:17.194Z",
+    "2023-06-08T14:11:59.604Z",
+    "2023-06-09T10:17:24.185Z",
+    "2023-06-10T09:15:04.904Z",
+    "2023-06-11T06:42:02.383Z",
+    "2023-06-12T06:31:17.178Z",
   ],
   currency: "USD",
-  locale: "en-US", // de-DE
+  locale: "en-US",
 };
 
 const account2 = {
-  owner: "Syed Irtaza Haider Zaidi",
+  owner: "Irtaza Haider Zaidi",
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 474,
   movementsDates: [
-    "2023-07-01T10:51:36.790Z",
-    "2023-07-03T23:36:17.929Z",
-    "2023-07-06T17:01:17.194Z",
-    "2023-07-08T14:11:59.604Z",
-    "2023-07-09T10:17:24.185Z",
-    "2023-07-10T09:15:04.904Z",
-    "2023-07-11T07:42:02.383Z",
-    "2023-07-12T21:31:17.178Z",
+    "2023-06-01T10:51:36.790Z",
+    "2023-06-03T23:36:17.929Z",
+    "2023-06-06T17:01:17.194Z",
+    "2023-06-08T14:11:59.604Z",
+    "2023-06-09T10:17:24.185Z",
+    "2023-06-10T09:15:04.904Z",
+    "2023-06-11T06:42:02.383Z",
+    "2023-06-12T21:31:17.178Z",
   ],
-  currency: "USD",
-  locale: "en-US",
+  currency: "PKR",
+  locale: "UR-pk",
 };
 
 const account3 = {
@@ -45,17 +45,17 @@ const account3 = {
   interestRate: 0.7,
   pin: 256,
   movementsDates: [
-    "2023-07-01T10:51:36.790Z",
-    "2023-07-03T23:36:17.929Z",
-    "2023-07-06T17:01:17.194Z",
-    "2023-07-08T14:11:59.604Z",
-    "2023-07-09T10:17:24.185Z",
-    "2023-07-10T09:15:04.904Z",
-    "2023-07-11T07:42:02.383Z",
-    "2023-07-12T21:31:17.178Z",
+    "2023-06-01T10:51:36.790Z",
+    "2023-06-03T23:36:17.929Z",
+    "2023-06-06T17:01:17.194Z",
+    "2023-06-08T14:11:59.604Z",
+    "2023-06-09T10:17:24.185Z",
+    "2023-06-10T09:15:04.904Z",
+    "2023-06-11T06:42:02.383Z",
+    "2023-06-12T21:31:17.178Z",
   ],
-  currency: "USD",
-  locale: "en-US",
+  currency: "PKR",
+  locale: "UR-pk",
 };
 
 const account4 = {
@@ -64,37 +64,38 @@ const account4 = {
   interestRate: 1,
   pin: 442,
   movementsDates: [
-    "2023-07-01T10:51:36.790Z",
-    "2023-07-03T23:36:17.929Z",
-    "2023-07-06T17:01:17.194Z",
-    "2023-07-08T14:11:59.604Z",
-    "2023-07-09T10:17:24.185Z",
+    "2023-06-01T10:51:36.790Z",
+    "2023-06-03T23:36:17.929Z",
+    "2023-06-06T17:01:17.194Z",
+    "2023-06-08T14:11:59.604Z",
+    "2023-06-09T10:17:24.185Z",
   ],
-  currency: "USD",
-  locale: "en-US",
+  currency: "PKR",
+  locale: "UR-pk",
 };
 
 const account5 = {
-  owner: "Syed Mustafa Ali",
+  owner: "Mustafa Ali",
   movements: [99, 32, 12, -600, 900, 200, -320, 400, 1100],
   interestRate: 0.9,
   pin: 482,
   movementsDates: [
     "2023-06-29T10:51:36.790Z",
-    "2023-07-01T10:51:36.790Z",
-    "2023-07-03T23:36:17.929Z",
-    "2023-07-06T17:01:17.194Z",
-    "2023-07-08T14:11:59.604Z",
-    "2023-07-09T10:17:24.185Z",
-    "2023-07-10T09:15:04.904Z",
-    "2023-07-11T07:42:02.383Z",
-    "2023-07-12T21:31:17.178Z",
+    "2023-06-01T10:51:36.790Z",
+    "2023-06-03T23:36:17.929Z",
+    "2023-06-06T17:01:17.194Z",
+    "2023-06-08T14:11:59.604Z",
+    "2023-06-09T10:17:24.185Z",
+    "2023-06-10T09:15:04.904Z",
+    "2023-06-11T06:42:02.383Z",
+    "2023-06-12T21:31:17.178Z",
   ],
-  currency: "USD",
-  locale: "en-US",
+  currency: "PKR",
+  locale: "UR-pk",
 };
 
 const accounts = [account1, account2, account3, account4, account5];
+let timer, sorted;
 
 // Elements Selection
 const labelWelcome = document.querySelector(".welcome");
@@ -135,21 +136,100 @@ const createUserNames = (theAccounts) => {
   });
 };
 
-let sorted;
 createUserNames(accounts);
+
+//Implementation of LogOutTimer.
+const logoutTimer = () => {
+  // set timer of 2.5 seconds
+  let totalTime = 60 * 0.5;
+  let minutes = String(Math.floor(totalTime / 60)).padStart(2, 0);
+  let seconds = String(Math.floor(totalTime % 60)).padStart(2, 0);
+
+  labelTimer.textContent = `${minutes}:${seconds}`;
+  totalTime--;
+
+  timer = setInterval(() => {
+    minutes = String(Math.floor(totalTime / 60)).padStart(2, 0);
+    seconds = String(Math.floor(totalTime % 60)).padStart(2, 0);
+
+    labelTimer.textContent = `${minutes}:${seconds}`;
+
+    if (totalTime === 0) {
+      clearInterval(timer);
+      containerApp.style.opacity = 0;
+      labelWelcome.textContent = `Log in to get started`;
+    }
+
+    totalTime--;
+  }, 1000);
+  return timer;
+};
+
+// Format the dates.
+const formatDates = (date) => {
+  // let day = String(date.getDate()).padStart(2, 0);
+  // let month = String(date.getMonth()).padStart(2, 0);
+  // let year = date.getFullYear();
+
+  return new Intl.DateTimeFormat(navigator.language).format(date);
+};
+
 let dateToday = new Date();
-let day = String(dateToday.getDate()).padStart(2, 0);
-let month = String(dateToday.getMonth()).padStart(2, 0);
-let year = dateToday.getFullYear();
+let formattedDate = formatDates(dateToday);
 let hours = dateToday.getHours();
 let minutes = dateToday.getMinutes();
 
-let dateAndTime = `${day}/${month}/${year}, ${String(hours).padStart(
-  2,
-  0
-)}:${String(minutes).padStart(2, 0)}`;
+let dateAndTime = `${formattedDate}, ${String(hours).padStart(2, 0)}:${String(
+  minutes
+).padStart(2, 0)}`;
 
-labelDate.textContent += dateAndTime;
+// labelDate.textContent = dateAndTime;
+
+// We can use API instead.
+const options = {
+  hour: "numeric",
+  minute: "numeric",
+  day: "numeric",
+  month: "numeric",
+  year: "numeric",
+};
+
+const locale = navigator.language;
+
+labelDate.textContent = new Intl.DateTimeFormat(locale, options).format(
+  new Date()
+);
+
+// Updates the time everyminute.
+setInterval(() => {
+  labelDate.textContent = new Intl.DateTimeFormat(locale, options).format(
+    new Date()
+  );
+}, 60000);
+
+//To calculate the days that has passed from today.
+const calculateDaysPassed = (date1, date2) => {
+  let numberOfDays = Math.round(
+    Math.abs(date2 - date1) / (1000 * 24 * 60 * 60)
+  );
+
+  if (numberOfDays === 0) {
+    return "Today";
+  } else if (numberOfDays === 1) {
+    return "Yesterday";
+  } else if (numberOfDays <= 7) {
+    return `${numberOfDays} days ago`;
+  } else {
+    return formatDates(date1);
+  }
+};
+
+//To display numbers in formatted according to the region of user.
+const formatNumbers = (amount) =>
+  new Intl.NumberFormat(currentAccount.locale, {
+    style: "currency",
+    currency: currentAccount.currency,
+  }).format(amount);
 
 // Display the movements made so far.
 const displayMovements = (account, isSorted = true) => {
@@ -164,18 +244,15 @@ const displayMovements = (account, isSorted = true) => {
     let type = movement > 0 ? "deposit" : "withdrawal";
 
     let movDate = new Date(account.movementsDates[i]);
-    let day = String(movDate.getDate()).padStart(2, 0);
-    let month = String(movDate.getMonth()).padStart(2, 0);
-    let year = movDate.getFullYear();
-
-    let movementsDate = `${day}/${month}/${year}`;
+    let movementsDate = calculateDaysPassed(movDate, new Date());
+    let formattedMovement = formatNumbers(movement);
 
     let code = `<div class="movements__row">
       <div class="movements__type movements__type--${type}">${
       i + 1
-    } ${type}</div>
+    }. ${type}</div>
       <div class="movements__date">${movementsDate}</div>
-      <div class="movements__value">${movement.toFixed(2)}€</div>
+      <div class="movements__value">${formattedMovement}</div>
     </div>`;
 
     containerMovements.insertAdjacentHTML("afterbegin", code);
@@ -189,7 +266,7 @@ const computeTotalBalance = (account) => {
     0
   );
 
-  labelBalance.textContent = `${totalBalance.toFixed(2)}€`;
+  labelBalance.textContent = `${formatNumbers(totalBalance)}`;
   account.totalBalance = totalBalance;
 };
 
@@ -199,34 +276,32 @@ const calculateSummary = (account) => {
     .filter((movement) => movement > 0)
     .reduce((accumulator, movement) => accumulator + movement, 0);
 
-  labelSumIn.textContent = `${deposits.toFixed(2)}€`;
+  labelSumIn.textContent = `${formatNumbers(deposits)}`;
 
   let withdrawals = account.movements
     .filter((movement) => movement < 0)
     .reduce((accumulator, movement) => accumulator + movement, 0);
 
-  labelSumOut.textContent = `${Math.abs(withdrawals).toFixed(2)}€`;
+  labelSumOut.textContent = `${formatNumbers(Math.abs(withdrawals))}`;
 
   let interest = account.movements
     .filter((movement) => movement >= 1)
     .map((deposit) => deposit * (account.interestRate / 100))
     .reduce((accumulator, movement) => accumulator + movement, 0);
 
-  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
+  labelSumInterest.textContent = `${formatNumbers(interest)}`;
 };
 
-// By default
 let currentAccount;
-// Creating a Fake Login By Default:
-// inputLoginUsername.value = "mz";
-// inputLoginPin.value = 244;
-// btnLogin.click();
-// console.log(currentAccount);
 
 // Login
 btnLogin.addEventListener("click", (e) => {
   e.preventDefault();
 
+  if (timer) {
+    clearInterval(timer);
+  }
+  timer = logoutTimer();
   sorted = false;
   currentAccount = accounts.find(
     (theAccount) =>
@@ -284,6 +359,10 @@ btnTransfer.addEventListener("click", (e) => {
     transferTo.movementsDates.push(new Date().toISOString());
 
     setTimeout(() => {
+      //Reset timer.
+      clearInterval(timer);
+      timer = logoutTimer();
+
       //Update Total Balance.
       computeTotalBalance(currentAccount);
 
@@ -296,10 +375,11 @@ btnTransfer.addEventListener("click", (e) => {
   }
 });
 
+// Loan
 btnLoan.addEventListener("click", (e) => {
   e.preventDefault();
 
-  let loanAmount = Math.floor(inputLoanAmount.value);
+  let loanAmount = +inputLoanAmount.value;
 
   if (
     loanAmount > 0 &&
@@ -309,6 +389,10 @@ btnLoan.addEventListener("click", (e) => {
     currentAccount.movementsDates.push(new Date().toISOString());
 
     setTimeout(() => {
+      //Reset timer.
+      clearInterval(timer);
+      timer = logoutTimer();
+
       //Update Total Balance.
       computeTotalBalance(currentAccount);
 
@@ -322,6 +406,7 @@ btnLoan.addEventListener("click", (e) => {
   inputLoanAmount.value = "";
 });
 
+// Account close.
 btnClose.addEventListener("click", (e) => {
   e.preventDefault();
 
@@ -343,6 +428,7 @@ btnClose.addEventListener("click", (e) => {
   inputClosePin.blur();
 });
 
+// Sort the movements.
 sorted = false;
 btnSort.addEventListener("click", (e) => {
   e.preventDefault();
